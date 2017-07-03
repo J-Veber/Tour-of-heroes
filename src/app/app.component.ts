74a41cae09fe17f263c0f623b919b2c60437455b
j-veber <kovalsky.corp@gmail.com>
2017-07-03 14:08:24 +1000

    lesson 1 done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,41 @@
 import { Component } from '@angular/core';
 
+export class Hero {
+  id: number;
+  name: string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  template: `
+    <h1>
+      {{title}}
+    </h1>
+    <h2>
+      {{hero.name}} details!
+    </h2>
+    <!--<div><label (click)="onLabelClick()" >id: </label>{{hero.id}}</div>-->
+    <div><label>id: </label>{{hero.id}}</div>
+    <div><label>name: </label><input [(ngModel)]="hero.name" placeholder="name"></div>
+    <!--<div><label>name: </label><input [(ngModel)]="hero.name" placeholder="name"></div>-->
+    `
 })
+
 export class AppComponent {
   title = 'app';
+  test = 231;
+  hero: Hero = {
+    id: 1,
+    name: 'azaza'
+  };
+  // constructor() {
+  //   setInterval( () => {
+  //     this.test = Math.floor(Math.random() * 100);
+  //   }, 1000)
+  // }
+  // onLabelClick() {
+  //   this.hero.name = 'clicked';
+  // }
 }
